bb2389c60d908c93e47c5d7a32d30018cd80bf0c
Marcos Issa <emaildoissa@gmail.com>
2023-06-26 12:42:39 -0300

    Implementacao botao salvar

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -7,36 +7,49 @@
  *
  * @author Adm
  */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
+
+    public void cadastrarProduto(ProdutosDTO produto) {
+
         //conn = new conectaDAO().connectDB();
-        
-        
+        try {
+            // Estabelecer conexão com o banco de dados
+            Connection conn = new conectaDAO().connectDB();
+
+            // Preparar a instrução SQL para inserir o produto no banco de dados
+            String sql = "INSERT INTO produtos (nome, valor) VALUES (?, ?)";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            // Definir os valores dos parâmetros na instrução SQL
+            stmt.setString(1, produto.getNome());
+            stmt.setDouble(2, produto.getValor());
+
+            // Executar a instrução SQL para inserir o produto no banco de dados
+            stmt.executeUpdate();
+
+            // Fechar a conexão com o banco de dados
+            conn.close();
+        } catch (SQLException e) {
+
+        }
+
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
